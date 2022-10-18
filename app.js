ead2d7469573851e19aa46d41bf0c56a20f71e3f
Zach Hughes <zach.hughes79@gmail.com>
2022-10-18 17:13:30 -0400

    testing server

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.static("assets"));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
-});
+    res.sendFile(__dirname + "/index.html")
+})
 
 let port = process.env.PORT;
 
