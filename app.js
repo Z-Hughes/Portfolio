f48e131e36758e9fcc9a01e5d83bdbea9a0bb719
Zach Hughes <zach.hughes79@gmail.com>
2022-10-18 17:25:33 -0400

    fixing server to display css

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("assets/css/main.css"));
+app.use(express.static(__dirname));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html")
